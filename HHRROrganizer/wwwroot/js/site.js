390720913cb77d4a127005800c3ad9c8611715ea
Asier Garper <asiergarper@gmail.com>
2021-06-26 09:22:38 +0200

    Added contextual help effects on some buttons using the popper library, to help the user to understand the functionalities of each button.

--- HHRROrganizer/wwwroot/js/site.js
+++ HHRROrganizer/wwwroot/js/site.js
@@ -1,7 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+//This function activates the functionality of the tooltips (help message) offered by bootstrap, for various buttons of the application.
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
 
 // Generate a random salary between 18000 and 32000, including both 18000 and 32000
 function generateRandomNumber(min, max) {
@@ -16,12 +20,11 @@ function randomDate(start, end) {
 document.querySelector('#randomGeneratorInput').addEventListener("click", function () {
 
     document.querySelector("#createEmployeeManually").style.display = "none";
-    document.querySelector("#successfullyCreatedEmployee").innerHTML = "Employee created succesfully";
+    document.querySelector("#successfullyCreatedEmployee").innerHTML = "Employee picture created succesfully.";
 
     axios.get(`https://randomuser.me/api/`)
         .then(function (response){
             let data = response.data.results[0]
-            console.log(data)
             //To enter the user image randomly, is something complex, we have to generate a hidden input in the front end with a boolean condition, to differentiate whether the image enters manually, or through the Fill Random user button via js.
             document.querySelector("#boolPicture").value = "True";
             //If we use the Fill Random User button, the variable changes to true and the UploadProfilePic method is executed differently.
@@ -42,7 +45,6 @@ document.querySelector('#randomGeneratorInput').addEventListener("click", functi
             document.querySelector("#randomGrossSalary").value = `${generateRandomNumber(20000, 34000)}`;
             document.querySelector("#randomNetSalary").value = `${generateRandomNumber(18000, 32000)}`;
             let randomDepartmentId = Math.floor(Math.random() * document.querySelectorAll("#randomDepartmentId option").length);
-            console.log(randomDepartmentId)
             document.querySelector("#randomDepartmentId").selectedIndex = randomDepartmentId;
 
             
